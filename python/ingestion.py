2dc5151355613ac7766be34bc628e6ecfe00adc4
Eduardo Bautista <eg.bb952@gmail.com>
2025-03-27 09:53:43 -0600

    extraction first try

--- python/ingestion.py
+++ python/ingestion.py
@@ -1,14 +1,34 @@
 import duckdb
 
 
-def main(csv_data):
-    data = duckdb.read_csv(csv_data, header=True, sep="|")
-
+def main_extraction(csv_data, table_name, connection):
+    
+    create_table = f"""
+    CREATE TABLE IF NOT EXISTS {table_name} (
+    timestamp DATETIME,
+    user_id VARCHAR,
+    session_key VARCHAR,
+    device_type VARCHAR,
+    campaign VARCHAR,
+    ad_set VARCHAR,
+    spend double,
+    utm_channel VARCHAR,
+    fbclid VARCHAR
+    )
+    """
+    
     query = f"""
+    INSERT OR REPLACE INTO {table_name}
     SELECT *
-    from {data}
+    FROM read_csv('{csv_data}', header = True, sep = '|')
     """
 
+    connection.sql(create_table)
+    connection.sql(query)
 
-
-csv_data = 'data/*.csv'
+if __name__ == "__main__":
+    con = duckdb.connect("database/project_database.db")
+    csv_data = 'data/facebook_clicks.csv'
+    table_name_1 = 'facebook_csv'
+    main_extraction(csv_data, table_name_1, con)
+    con.close()
